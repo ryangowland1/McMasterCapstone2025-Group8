35bb12d7e1d532fa685baf440d28e576e95935b0
ryangowland <ryangowland@ROSCapstone1.myguest.virtualbox.org>
2025-02-01 19:46:33 -0500

    Added ROS module

---
+++ Program/Main_Code/parking_ws/build/autonomous_parking/CMakeFiles/Perception.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ryangowland/parking_ws/devel/lib/autonomous_parking/Perception"
+  "/home/ryangowland/parking_ws/devel/lib/autonomous_parking/Perception.pdb"
+  "CMakeFiles/Perception.dir/src/Perception.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Perception.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
