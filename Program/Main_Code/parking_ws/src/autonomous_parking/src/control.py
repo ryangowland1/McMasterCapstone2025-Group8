90089a3134cce711ac16a9e197e3d71b81da814b
6mb11 <45149917+6MB11@users.noreply.github.com>
2025-04-24 13:20:32 -0400

    making simulator more realistic, ensuring control resets speed and accel to 0 on interrupt

--- Program/Main_Code/parking_ws/src/autonomous_parking/src/control.py
+++ Program/Main_Code/parking_ws/src/autonomous_parking/src/control.py
@@ -34,9 +34,12 @@ class ControlNode:
         self.odom_msg = Odometry()
         self.odom_zero = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
+        simulated_wheelbase = 0.3 # set to 0.3 for physical opeeration and 1.25 for simulated
+        physical_wheelbase = 0.3
+
         # Tuning parameters
-        self.lookahead_dist = 0.3 #0.7
-        self.max_speed = 0.5  # m/s
+        self.lookahead_dist = 0.3 * simulated_wheelbase / physical_wheelbase
+        self.max_speed = 0.5 * simulated_wheelbase / physical_wheelbase  # m/s
         self.aeb_ttc_threshold = 0.5  # seconds
 
         # Subscriptions
@@ -106,95 +109,90 @@ class ControlNode:
 
     def run(self):
         # rate = rospy.Rate(50)  # 50 Hz control loop
+        
+        try:
+            while not rospy.is_shutdown():
+                # All this needs to be run wish an updated trajectory and odom_msg so can't be in either callback
+                # Extract current pose
+                x = self.odom_msg.pose.pose.position.x
+                y = self.odom_msg.pose.pose.position.y
+                z = self.odom_msg.pose.pose.position.z
 
-        while not rospy.is_shutdown():
+                orientation = self.odom_msg.pose.pose.orientation
+                (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+                    [orientation.x, orientation.y,
+                        orientation.z, orientation.w]
+                )
 
-            # All this needs to be run wish an updated trajectory and odom_msg so can't be in either callback
-            # Extract current pose
-            x = self.odom_msg.pose.pose.position.x
-            y = self.odom_msg.pose.pose.position.y
-            z = self.odom_msg.pose.pose.position.z
+                # Path Planning resets its frame of reference each time there's a new path so Control needs to do that too for consistency
+                if self.trajectory != self.prev_trajectory:
+                    self.odom_zero = [x, y, z, roll, pitch, yaw]
 
-            orientation = self.odom_msg.pose.pose.orientation
-            (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
-                [orientation.x, orientation.y, orientation.z, orientation.w]
-            )
+                    # There's a new path with new waypoints so can reset the waypoint indexer
+                    # 0th waypoint is current position so start at 1
+                    self.current_waypoint_idx = 1
+                    self.prev_trajectory = self.trajectory
+                    self.first_time = False
 
-            # Path Planning resets its frame of reference each time there's a new path so Control needs to do that too for consistency
-            if self.trajectory != self.prev_trajectory:
-                self.odom_zero = [x, y, z, roll, pitch, yaw]
+                # Set position to be relative to frame of reference. For an explaniation, see https://medium.com/@parkie0517/rigid-transformation-in-3d-space-translation-and-rotation-d701d8859ba8 or your MECHENG 4K03 notes
+                translation = np.array([[1, 0, 0, -self.odom_zero[0]], [
+                                        0, 1, 0, -self.odom_zero[1]], [0, 0, 1, -self.odom_zero[2]], [0, 0, 0, 1]])
+                rotation_roll = np.array([[1, 0, 0, 0], [0, math.cos(-self.odom_zero[3]), -math.sin(-self.odom_zero[3]), 0], [
+                                          0, math.sin(-self.odom_zero[3]), math.cos(-self.odom_zero[3]), 0], [0, 0, 0, 1]])
+                rotation_pitch = np.array([[math.cos(-self.odom_zero[4]), 0, math.sin(-self.odom_zero[4]), 0], [
+                                           0, 1, 0, 0], [-math.sin(-self.odom_zero[4]), 0, math.cos(-self.odom_zero[4]), 0], [0, 0, 0, 1]])
+                rotation_yaw = np.array([[math.cos(-self.odom_zero[5]), -math.sin(-self.odom_zero[5]), 0, 0], [
+                                         math.sin(-self.odom_zero[5]), math.cos(-self.odom_zero[5]), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 
-                # There's a new path with new waypoints so can reset the waypoint indexer
-                # 0th waypoint is current position so start at 1
-                self.current_waypoint_idx = 1
-                self.prev_trajectory = self.trajectory
-                self.first_time = False
+                point_new_coord_sys = np.dot(rotation_roll, np.dot(rotation_pitch, np.dot(
+                    rotation_yaw, np.dot(translation, np.array([[x], [y], [z], [1]])))))
 
-            # Set position to be relative to frame of reference. For an explaniation, see https://medium.com/@parkie0517/rigid-transformation-in-3d-space-translation-and-rotation-d701d8859ba8 or your MECHENG 4K03 notes
-            translation = np.array([[1, 0, 0, -self.odom_zero[0]], [
-                                    0, 1, 0, -self.odom_zero[1]], [0, 0, 1, -self.odom_zero[2]], [0, 0, 0, 1]])
-            rotation_roll = np.array([[1, 0, 0, 0], [0, math.cos(-self.odom_zero[3]), -math.sin(-self.odom_zero[3]), 0], [
-                                      0, math.sin(-self.odom_zero[3]), math.cos(-self.odom_zero[3]), 0], [0, 0, 0, 1]])
-            rotation_pitch = np.array([[math.cos(-self.odom_zero[4]), 0, math.sin(-self.odom_zero[4]), 0], [
-                                       0, 1, 0, 0], [-math.sin(-self.odom_zero[4]), 0, math.cos(-self.odom_zero[4]), 0], [0, 0, 0, 1]])
-            rotation_yaw = np.array([[math.cos(-self.odom_zero[5]), -math.sin(-self.odom_zero[5]), 0, 0], [
-                                     math.sin(-self.odom_zero[5]), math.cos(-self.odom_zero[5]), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+                x = point_new_coord_sys[0, 0]
+                # Moving start point relative to detected points, path planning moves points relative to starting position, so opposite sign compared to path planning
+                y = point_new_coord_sys[1, 0] + START_Y
+                yaw = yaw - self.odom_zero[5]
 
-            point_new_coord_sys = np.dot(rotation_roll, np.dot(rotation_pitch, np.dot(
-                rotation_yaw, np.dot(translation, np.array([[x], [y], [z], [1]])))))
+                self.current_pose = (x, y, yaw)
 
-            x = point_new_coord_sys[0, 0]
-            # Moving start point relative to detected points, path planning moves points relative to starting position, so opposite sign compared to path planning
-            y = point_new_coord_sys[1, 0] + START_Y
-            yaw = yaw - self.odom_zero[5]
-
-            self.current_pose = (x, y, yaw)
-            if self.aeb_stop:
-                # emergency stop
-                speed_cmd = 0.0
-                steer_cmd = 0.0
-
-                # Publish final commands
-                msg = AckermannDriveStamped()
-                msg.drive.speed = speed_cmd
-                msg.drive.acceleration = 0
-                msg.drive.steering_angle = steer_cmd
-                msg.drive.steering_angle_velocity = 0
-                # rospy.loginfo("Speed: %f m/s. Steering %f rad.", speed_cmd, steer_cmd)
-                self.cmd_pub.publish(msg)
-
-            else:
                 # Follow the path if not done
                 if self.current_waypoint_idx < len(self.trajectory):
 
                     target = self.trajectory[self.current_waypoint_idx]
-                    speed_cmd, steer_cmd = self.pure_pursuit_control(target)
+                    # Check if we reached the waypoint
+                    dx = target[0] - self.current_pose[0]
+                    dy = target[1] - self.current_pose[1]
+                    dist_to_wp = math.hypot(dx, dy)
+
+                    # CHANGE AS NEEDED, must be high enough to prevent a waypoint from being driven past
+                    while dist_to_wp < self.lookahead_dist:
+                        self.current_waypoint_idx += 1
+
+                        if (self.current_waypoint_idx == len(self.trajectory)):
+
+                            # For simulation since shutting off when final point is within lookahead_dist is too soon and real car moves parking spot backwards anyways so stopping is later
+                            # if dist_to_wp > self.lookahead_dist / 4:
+                                # self.current_waypoint_idx -= 1
+                                # target = self.trajectory[self.current_waypoint_idx]
+                            break
+
+                        target = self.trajectory[self.current_waypoint_idx]
+                        # Check if we reached the waypoint
+                        dx = target[0] - self.current_pose[0]
+                        dy = target[1] - self.current_pose[1]
+                        dist_to_wp = math.hypot(dx, dy)
+
+                    speed_cmd, steer_cmd = self.pure_pursuit_control(
+                        target)
 
                     # Publish final commands
                     msg = AckermannDriveStamped()
                     msg.drive.speed = speed_cmd
-                    msg.drive.acceleration = 0.5
+                    msg.drive.acceleration = 0.5 * 1.25 / 0.3
                     msg.drive.steering_angle = steer_cmd
                     msg.drive.steering_angle_velocity = 1
                     # rospy.loginfo("Speed: %f m/s. Steering %f rad.", speed_cmd, steer_cmd)
                     self.cmd_pub.publish(msg)
 
-                    # Check if we reached the waypoint
-                    dx = target[0] - self.current_pose[0]
-                    dy = target[1] - self.current_pose[1]
-                    dist_to_wp = math.hypot(dx, dy)
-                    
-                    while dist_to_wp < self.lookahead_dist:  # CHANGE AS NEEDED, must be high enough to prevent a waypoint from being driven past
-                        self.current_waypoint_idx += 1
-                        
-                        if (self.current_waypoint_idx == len(self.trajectory)):
-                            break
-                        
-                        target = self.trajectory[self.current_waypoint_idx]
-                        dx = target[0] - self.current_pose[0]
-                        dy = target[1] - self.current_pose[1]
-                        dist_to_wp = math.hypot(dx, dy)
-                        
                 else:
                     # Done
                     speed_cmd = 0.0
@@ -209,7 +207,22 @@ class ControlNode:
                     # rospy.loginfo("Speed: %f m/s. Steering %f rad.", speed_cmd, steer_cmd)
                     self.cmd_pub.publish(msg)
 
-            # rate.sleep()
+                # rate.sleep()
+
+        except KeyboardInterrupt:
+            # emergency stop
+            speed_cmd = 0.0
+            steer_cmd = 0.0
+
+            # Publish final commands
+            msg = AckermannDriveStamped()
+            msg.drive.speed = speed_cmd
+            msg.drive.acceleration = 0
+            msg.drive.steering_angle = steer_cmd
+            msg.drive.steering_angle_velocity = 0
+            # rospy.loginfo("Speed: %f m/s. Steering %f rad.", speed_cmd, steer_cmd)
+            self.cmd_pub.publish(msg)
+            sys.exit(0)
 
     # Pure pursuit control assumes constant speed and controls the steering
     def pure_pursuit_control(self, target):
@@ -232,7 +245,7 @@ class ControlNode:
         # print(x_c, y_c, x_t, y_t, heading_error)
 
         # Steering
-        k_steering = 1.0 #2.5 #CHANGE AS NEEDED
+        k_steering = 0.8 # CHANGE AS NEEDED
         steering_cmd = k_steering * heading_error
 
         # Speed depends on heading error
@@ -242,6 +255,7 @@ class ControlNode:
 
 
 if __name__ == "__main__":
-    rospy.init_node("control_node")
+    rospy.init_node("control_node", disable_signals=True) # disable_signals needed for KeyboardInterrupt
     node = ControlNode()
     node.run()
+
